c6400da8205518be03b8bf20ab9ef047965546ef
unknown <Samy@Ordinatueur.(none)>
2012-05-22 16:38:53 +0200

    viré les packages

--- Confirmation.java
+++ Confirmation.java
@@ -2,8 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package bridge;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import javax.swing.AbstractAction;
