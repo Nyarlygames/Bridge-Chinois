86094878f3d4759e0f4116d98ec7b2745e323cfe
unknown <Samy@Ordinatueur.(none)>
2012-05-24 18:08:16 +0200

    ajout du menu dans graphique samy

--- Multi.java
+++ Multi.java
@@ -4,9 +4,7 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.URL;
-import java.net.UnknownHostException;
 
 /*
  * To change this template, choose Tools | Templates
