c9dd79c20aa0676da458f805c0ec3cebb31e64b5
unknown <utilisateur@R610.(none)>
2012-05-30 18:05:16 +0200

    ajout des nouveaux bouton

--- VSIA.java
+++ VSIA.java
@@ -3,8 +3,10 @@
  * and open the template in the editor.
  */
 
-
 import java.awt.Color;
+import java.util.Observable;
+import java.util.Observer;
+
 import javax.swing.SwingUtilities;
 
 /**
@@ -36,26 +38,19 @@ public class VSIA extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         nbParties = new javax.swing.JComboBox();
         mode = new javax.swing.JComboBox();
-        Slide = new javax.swing.JSlider();
+        jSlider1 = new javax.swing.JSlider();
         facile = new javax.swing.JLabel();
         moyen = new javax.swing.JLabel();
         difficile = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setTitle("Jouer");
         setResizable(false);
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowClosing(java.awt.event.WindowEvent evt) {
-                formWindowClosing(evt);
-            }
-        });
         getContentPane().setLayout(null);
 
-        lbDif.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         lbDif.setText("Difficulté :");
         getContentPane().add(lbDif);
-        lbDif.setBounds(27, 107, 60, 14);
+        lbDif.setBounds(27, 107, 58, 16);
 
         Launch.setText("Démarrer");
         Launch.addActionListener(new java.awt.event.ActionListener() {
@@ -64,7 +59,7 @@ public class VSIA extends javax.swing.JFrame {
             }
         });
         getContentPane().add(Launch);
-        Launch.setBounds(211, 208, 100, 23);
+        Launch.setBounds(211, 208, 100, 25);
 
         Cancel.setText("Annuler");
         Cancel.addActionListener(new java.awt.event.ActionListener() {
@@ -73,10 +68,9 @@ public class VSIA extends javax.swing.JFrame {
             }
         });
         getContentPane().add(Cancel);
-        Cancel.setBounds(46, 208, 80, 23);
+        Cancel.setBounds(46, 208, 80, 25);
 
         jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        jPanel1.setOpaque(false);
 
         nbParties.setEditable(true);
         nbParties.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "3", "5", "10" }));
@@ -102,7 +96,7 @@ public class VSIA extends javax.swing.JFrame {
                 .addComponent(mode, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(nbParties, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(29, Short.MAX_VALUE))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -115,96 +109,44 @@ public class VSIA extends javax.swing.JFrame {
         );
 
         getContentPane().add(jPanel1);
-        jPanel1.setBounds(27, 26, 240, 42);
+        jPanel1.setBounds(27, 26, 260, 42);
 
-        Slide.setMaximum(3);
-        Slide.setMinimum(1);
-        Slide.setMinorTickSpacing(1);
-        Slide.setSnapToTicks(true);
-        Slide.setToolTipText("");
-        Slide.setValue(2);
-        Slide.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        getContentPane().add(Slide);
-        Slide.setBounds(46, 139, 273, 18);
+        jSlider1.setMaximum(3);
+        jSlider1.setMinimum(1);
+        jSlider1.setMinorTickSpacing(1);
+        jSlider1.setSnapToTicks(true);
+        jSlider1.setToolTipText("");
+        jSlider1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        getContentPane().add(jSlider1);
+        jSlider1.setBounds(46, 139, 273, 18);
 
-        facile.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         facile.setText("Facile");
         getContentPane().add(facile);
-        facile.setBounds(46, 168, 59, 14);
+        facile.setBounds(46, 168, 59, 16);
 
-        moyen.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         moyen.setText("Moyen");
         getContentPane().add(moyen);
-        moyen.setBounds(169, 168, 59, 14);
+        moyen.setBounds(169, 168, 59, 16);
 
-        difficile.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         difficile.setText("Difficile");
         getContentPane().add(difficile);
-        difficile.setBounds(285, 168, 50, 14);
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/menu.jpg"))); // NOI18N
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(0, 0, 350, 270);
+        difficile.setBounds(285, 168, 50, 16);
 
         java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        setBounds((screenSize.width-354)/2, (screenSize.height-295)/2, 354, 295);
+        setBounds((screenSize.width-359)/2, (screenSize.height-295)/2, 359, 295);
     }// </editor-fold>//GEN-END:initComponents
 
     private void CancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CancelActionPerformed
         // TODO add your handling code here:
-     //   Confirmation a = new Confirmation(this, true, "Revenir au menu ?");
-     //   a.setVisible(true);
+        //   Confirmation a = new Confirmation(this, true, "Revenir au menu ?");
+        //   a.setVisible(true);
 
-   //     if (a.getReturnStatus() == 1) {
-            Menu m = new Menu();
-            m.setVisible(true);
-            this.dispose();
-      //  }
-    }//GEN-LAST:event_CancelActionPerformed
-
-    private void LaunchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LaunchActionPerformed
-        // TODO add your handling code here:
-        
-        // on ferme la fenetre de menu
-        this.dispose();
-        
-        Table table = new Table();
-		Moteur moteur = new Moteur(table);
-		Jeu monJeu = new Jeu(moteur,2);
-   		Graphique gg = new Graphique(monJeu);
-        SwingUtilities.invokeLater(gg);
-        
-		System.out.println("j'ai initialise mon paquet melange");
-		monJeu.moteur.table.paquet.afficherPaquetConsole();
-		
-		monJeu.initialiser();
-		System.out.println("apres destribution le paquet dois Ãªtre vide :");
-		if(monJeu.moteur.table.paquet.estVide())
-			System.out.println("VRAI");
-		else
-			System.out.println("FAUX");
-		
-		System.out.println("j'affiche la premiÃ¨re main");
-		monJeu.moteur.table.main1.afficherMainConsole();
-		
-		System.out.println("j'affiche la deusiÃ¨me main");
-		monJeu.moteur.table.main2.afficherMainConsole();
-		
-		System.out.println("j'affiche les 6 piles de 5 cartes :");
-		for(int i=0;i<6;i++)
-		{
-			System.out.println("j'affiche la pile num " + i);
-			monJeu.moteur.table.piles.get(i).afficherPileConsole();
-		}
-		
-    }//GEN-LAST:event_LaunchActionPerformed
-
-    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
-        // TODO add your handling code here:
+        //     if (a.getReturnStatus() == 1) {
         Menu m = new Menu();
         m.setVisible(true);
         this.dispose();
-    }//GEN-LAST:event_formWindowClosing
+        //  }
+    }//GEN-LAST:event_CancelActionPerformed
 
     private void modeItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_modeItemStateChanged
         // TODO add your handling code here:
@@ -226,7 +168,6 @@ public class VSIA extends javax.swing.JFrame {
 
     private void nbPartiesFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_nbPartiesFocusLost
         // TODO add your handling code here:
-        System.out.println("protu");
         if (nbParties.getSelectedItem() instanceof Integer) {
             nbParties.setBackground(Color.white);
 
@@ -237,6 +178,54 @@ public class VSIA extends javax.swing.JFrame {
 
         }
     }//GEN-LAST:event_nbPartiesFocusLost
+
+    private void LaunchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LaunchActionPerformed
+        // TODO add your handling code here:
+        // on ferme la fenetre de menu
+        this.dispose();
+        new Thread(new Runnable() {
+        	public void run() {
+              
+		        Table table = new Table();
+		        Moteur moteur = new Moteur(table);
+		        //Jeu monJeu = new Jeu(moteur, 1,0,1,0);      
+		        Jeu monJeu = new Jeu(moteur, 1,mode.getSelectedIndex() , Integer.parseInt((String)nbParties.getSelectedItem()), jSlider1.getValue());
+
+		        final Graphique gg = new Graphique(monJeu, 1);
+		        // test
+		        monJeu.addObservateur(new Observateur() {
+					public void update(Jeu jeu) {
+						gg.getZoneDessin().repaint();					
+					}
+				});
+		        SwingUtilities.invokeLater(gg);
+		        monJeu.jouer();
+		        /*monJeu.moteur.table.paquet.afficherPaquetConsole();
+		        /*System.out.println("apres destribution le paquet dois etre vide :");
+		        if (monJeu.moteur.table.paquet.estVide()) {
+		            System.out.println("VRAI");
+		        } else {
+		            System.out.println("FAUX");
+		        }
+		
+		        System.out.println("j'affiche la premiere main");
+		        monJeu.moteur.table.main1.afficherMainConsole();
+		
+		        System.out.println("j'affiche la deusiÃ¨me main");
+		        monJeu.moteur.table.main2.afficherMainConsole();
+		
+		        System.out.println("j'affiche les 6 piles de 5 cartes :");
+		        for (int i = 0; i < 6; i++) {
+		            System.out.println("j'affiche la pile num " + i);
+		            monJeu.moteur.table.piles.get(i).afficherPileConsole();
+		        }*/
+		        //System.out.println(monJeu.getMoteur().getTable().getAtout());
+
+		      }
+        }).start();
+
+
+    }//GEN-LAST:event_LaunchActionPerformed
 
     /**
      * @param args the command line arguments
@@ -282,12 +271,11 @@ public class VSIA extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Cancel;
     private javax.swing.JButton Launch;
-    private javax.swing.JSlider Slide;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel difficile;
     private javax.swing.JLabel facile;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JSlider jSlider1;
     private javax.swing.JLabel lbDif;
     private javax.swing.JComboBox mode;
     private javax.swing.JLabel moyen;
