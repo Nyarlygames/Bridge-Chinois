d2bc31fe726e78ddf711b4992eeebdafc4557cfa
amine38 <oueldbled@hotmail.fr>
2012-05-31 17:09:54 +0200

    correction TestJeu (contour bouton)
    Signed-off-by: amine38 <oueldbled@hotmail.fr>

--- TestJeu.java
+++ TestJeu.java
@@ -7,6 +7,22 @@ Date de Derniere modification 12/05/2012 : 18:54
 public class TestJeu {
 
     public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
         Menu m = new Menu();
         m.setVisible(true);
     }
